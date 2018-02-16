03a4268387ed78b4f81efd791bb78a96f043fd1a
OndraX <xtkaon01@gjk.cz>
2018-02-16 10:31:58 +0000

    Initial commit.

---
+++ js/base.js
@@ -0,0 +1,31 @@
+var body = $('html, body');
+
+
+	$('#togglebar-line').click(function(){
+		$e = this;
+		console.log($e.parentNode.classList);
+		$e.parentNode.classList.toggle('hide-the-kids');
+		}
+		);
+
+	function scrollTo(element){
+		var ypos = element.offset().top;
+		console.log(ypos);
+
+		body.stop().animate({scrollTop:ypos},
+				300,
+				'swing',
+			       	function(){
+					console.log('finito')
+				});
+	}
+
+	function mHideHead(){
+	scrollTo($('#page-head'));
+
+	document.body.classList.toggle('scrolled-past'); //TODO: polyfill
+	
+	}
+
+		$('#body-butt').click(mHideHead);
+		/*TODO: the same on overscroll*/
